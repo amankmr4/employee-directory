3adfb4f123e552dbc1bbcc14ce922621d92723ad
Aman Kumar <aman.kmr4@yahoo.com>
2021-01-12 00:01:11 +1100

    added the basic layout without css for this app

---
+++ src/components/SearchBar/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function SearchBar() {
+
+    return (
+        <form>
+            <label>Search For Name</label>
+            <input type="text">
+            </input>
+        </form>
+    )
+
+}
+
+export default SearchBar;
---
+++ src/components/Title/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+
+function Title() {
+    return (
+        <div>
+            <h1>Employee Directory</h1>
+            <p>here you can find the emoplyee below</p>
+        </div>
+    );
+}
+
+export default Title;
