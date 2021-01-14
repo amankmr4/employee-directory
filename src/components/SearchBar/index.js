34750e734478209fe2cbacdac4856fd3636321df
Aman Kumar <aman.kmr4@yahoo.com>
2021-01-14 23:23:15 +1100

    added the node package gh-pages for deployment

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -3,7 +3,7 @@ import React from "react";
 function SearchBar(props) {
 
     return (
-        <form className="text-center">
+        <form className="text-center mb-2 mt-0">
             <label className="mr-2">Search by name</label>
             <input type="text" onChange={props.handleInput}>
             </input>
--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,12 +4,12 @@ import React from "react";
 
 function Title() {
     return (
-        <div>
-            <div className="jumbotron">
-                <h1 className="display-4 text-center">Employee Directory</h1>
-                <p className="lead text-center">here you can find the emoplyee below</p>
-            </div>
+
+        <div className="jumbotron bg-primary">
+            <h1 className="display-4 text-center">Employee Directory</h1>
+            <p className="lead text-center">here you can find the emoplyee below you and also click on the names title to sort by name order</p>
         </div>
+
     );
 }
 
